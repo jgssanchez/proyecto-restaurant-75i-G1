1a06f3e60c7512539a56ac650e7688880037a1bb
Julio Sanchez <jgs.sanchez94@gmail.com>
2024-03-29 17:36:56 -0300

    agregue footer y un .css para darle su estilo

--- src/components/common/footer.jsx
+++ src/components/common/footer.jsx
@@ -1,9 +1,51 @@
+import "bootstrap/dist/css/bootstrap.css";
+import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+import '../common/footer.css'; 
+
 const Footer = () => {
-    return (
-        <footer className="text-center bg-green text-light py-4">
-            <p>&copy; Todos los derechos reservados</p>
-        </footer>
-    );
+  return (
+    <footer className="footer">
+      <Container>
+        <Row>
+          <Col md={3} className="footer-column">
+            <img src="logo.png" alt="Logo" className="footer-logo" />
+            <p>&copy;  RollingCode Todos los derechos reservados</p>
+            <p>Proyecto final</p>
+            <p>comisión 75-i Grupo 1</p>
+          </Col>
+          <Col md={3} className="footer-column">
+            <h5>Información</h5>
+            <p>Dirección: Gral. Paz 576, T4000 San Miguel de Tucumán, Tucumán</p>
+            <p>Teléfono:  +543815783030</p>
+            <p>Email: academy@rollingcodeschool.com</p>
+          </Col>
+          <Col md={3} className="footer-column">
+            <h5>Redes Sociales</h5>
+            <ul className="list-unstyled">
+              <li>
+                <a href="https://www.facebook.com/">Facebook</a>
+              </li>
+              <li>
+                <a href="https://www.twitter.com/">Twitter</a>
+              </li>
+              <li>
+                <a href="https://www.instagram.com/">Instagram</a>
+              </li>
+            </ul>
+          </Col>
+          <Col md={3} className="footer-column">
+            <h5>Contacto</h5>
+            <p>Envíanos un mensaje</p>
+            <a href="mailto:contacto@example.com">contacto@example.com</a>
+          </Col>
+        </Row>
+      </Container>
+    </footer>
+  );
 };
 
 export default Footer;
+
+
+
